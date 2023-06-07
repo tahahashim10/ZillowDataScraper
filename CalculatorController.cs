7bfe3f810cc60b0c82eccae038de768ea2293e44
tahahashim10 <128276249+tahahashim10@users.noreply.github.com>
2023-06-06 20:10:20 -0400

    Add files via upload

---
+++ CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using BasicCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View(new Calculator());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(Calculator c,String calculate)
+        {
+           if(calculate == "add")
+            {
+                c.TotalNum = c.FirstNum + c.SecondNum;
+            }
+            else if (calculate == "min")
+            {
+                c.TotalNum = c.FirstNum - c.SecondNum;
+            }
+            else if (calculate == "mul")
+            {
+                c.TotalNum = c.FirstNum * c.SecondNum;
+            }
+            else
+            {
+                c.TotalNum = c.FirstNum / c.SecondNum;
+            }
+
+            return View(c);
+        }
+
+    }
+}
